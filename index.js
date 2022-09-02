a75e1ed80009b2a8f8d3864f88a28be5d4c5bfc6
Anslie <brantae@gmail.com>
2022-09-01 21:37:46 -0500

    commit tree a3ca58a6da7215f3e1e7329a57183453ccf53ece

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+
+        },
+        body: JSON.stringify({
+            "name": name,
+             "email": email,
+            }),
+    }
+    )
+    .then(res => res.json())
+    .then(newObj => document.querySelector('body').innerHTML = newObj.id)
+    .catch(error => document.querySelector('body').innerHTML = error)
+    
+
+}
+
